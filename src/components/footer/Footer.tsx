4efda8b0e17b4c6ee77af9477acb98c8c87d83ad
ashiq-rahman07 <web3.0.ashiq@gmail.com>
2025-02-22 21:00:25 +0600

    again check

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Chhatak, Sylhet</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+8801727504474</p>
                 </div>
               </div>
             </div>
