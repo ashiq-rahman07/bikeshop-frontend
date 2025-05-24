dd44284aabf4abb32b76f6696bf22f3b319adfc3
ashiq-rahman07 <web3.0.ashiq@gmail.com>
2025-05-24 11:49:15 +0600

    base api updatet

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -7,7 +7,7 @@ import { RootState } from '../store';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v1',
+    baseUrl: 'https://bikeshop-server.onrender.com/api/v1',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
